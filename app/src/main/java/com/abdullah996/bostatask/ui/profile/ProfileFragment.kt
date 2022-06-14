a758cfe0b192409b73e132f6d53e99c455fd4074
abdullaheladly <abdullaheladly@gmail.com>
2022-06-14 21:55:53 +0200

    finish working on profile screen

--- app/src/main/java/com/abdullah996/bostatask/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/abdullah996/bostatask/ui/profile/ProfileFragment.kt
@@ -1,5 +1,8 @@
 package com.abdullah996.bostatask.ui.profile
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -34,15 +37,47 @@ class ProfileFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         _binding=FragmentProfileBinding.inflate(layoutInflater,container,false)
-        getRandomUser()
+       handleNoInternetConnection()
         setupAdapter()
+        setupSwipe()
         return binding.root
+    }
+
+    private fun setupSwipe() {
+        binding.sToRefresh.setOnRefreshListener {
+            binding.sToRefresh.isRefreshing =true
+           handleNoInternetConnection()
+
+        }
+    }
+
+
+    private fun handleNoInternetConnection(){
+        if (hasInternetConnection()){
+            getRandomUser()
+            binding.rvAlbums.visibility=View.VISIBLE
+            binding.txtError.visibility=View.GONE
+            binding.txtName.visibility=View.VISIBLE
+            binding.txtAddress.visibility=View.VISIBLE
+
+
+        }
+        else{
+            binding.rvAlbums.visibility=View.GONE
+            binding.txtError.visibility=View.VISIBLE
+            binding.txtName.visibility=View.GONE
+            binding.txtAddress.visibility=View.GONE
+            Toast.makeText(requireContext(), "No Internet connection", Toast.LENGTH_SHORT).show()
+            binding.sToRefresh.isRefreshing=false
+        }
     }
 
     private fun setupAdapter() {
         binding.rvAlbums.adapter=albumsAdapter
         binding.rvAlbums.layoutManager=LinearLayoutManager(requireContext())
-
+        /*binding.rvAlbums.itemAnimator=SlideInLeftAnimator().apply {
+            addDuration=500
+        }*/
     }
 
     private fun getRandomUser() {
@@ -53,10 +88,12 @@ class ProfileFragment : Fragment() {
                         usersList=it.data
                         getUserAlbum()
                     }
+                    binding.sToRefresh.isRefreshing=false
 
                 }
                 ApiStatus.ERROR -> {
                     Toast.makeText(requireContext(), it.message.toString(), Toast.LENGTH_SHORT).show()
+                    binding.sToRefresh.isRefreshing=false
 
                 }
                 ApiStatus.LOADING -> {
@@ -68,6 +105,10 @@ class ProfileFragment : Fragment() {
 
     private fun getUserAlbum() {
        val user= usersList.random()
+        binding.txtName.text=user.name
+
+        val address="${user.address?.city} ${user.address?.street} ${user.address?.zipcode} "
+        binding.txtAddress.text=address
         user.id?.let { userId->
             profileViewModel.getUserAlbums(userId).observe(viewLifecycleOwner) {
                 when (it.status) {
@@ -88,6 +129,19 @@ class ProfileFragment : Fragment() {
                     }
                 }
             }
+        }
+    }
+    private fun hasInternetConnection():Boolean{
+        val connectivityManager=requireActivity().getSystemService(
+            Context.CONNECTIVITY_SERVICE
+        ) as ConnectivityManager
+        val activeNetwork=connectivityManager.activeNetwork?:return false
+        val capabilities=connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
+        return  when{
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ->true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            else -> false
         }
     }
 
